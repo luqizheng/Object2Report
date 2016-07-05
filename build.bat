ce7903a143410ce9de31089427b0208b0675938f
卢骐征 <cadylu@126.com>
2016-07-05 17:19:30 +0800

    修正编译目录

--- build.bat
+++ build.bat
@@ -1,4 +1,4 @@
-dotnet build src/Coder.Object2Report.Excel -o ./Bin/net461 -c:Release --framework:net461
-dotnet build src/Coder.Object2Report.Excel -o ./Bin/net4 -c:Release --framework:net4
+dotnet build src/Coder.Object2Report.Renders.NPOI -o ./Bin/net461 -c:Release --framework:net461
+dotnet build src/Coder.Object2Report.Renders.NPOI -o ./Bin/net4 -c:Release --framework:net4
 
 rem dotnet build src/Coder.Object2Report.Excel -o ./Bin/net461 -c:Release --framework:net461
--- pack.release.bat
+++ pack.release.bat
@@ -1,2 +1,2 @@
 dotnet pack src/Coder.Object2Report -c:release -o:./
-dotnet pack src/Coder.Object2Report.Excel -c:release -o:./
+dotnet pack src/Coder.Object2Report.Renders.NPOI -c:release -o:./
