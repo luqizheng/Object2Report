7f62a5e5c766e50ef6d60e2e2e9514acac731767
nil <nil-nothing>
2019-02-22 09:40:32 +0800

    版本信息

--- pack.release.bat
+++ pack.release.bat
@@ -1,5 +1,4 @@
 dotnet restore src/Coder.Object2Report
 dotnet restore src/Coder.Object2Report.Renders.NPOI
-
-dotnet pack src/Coder.Object2Report -c:release -o:%cd%
 dotnet pack src/Coder.Object2Report.Renders.NPOI -c:release -o:%cd%
+dotnet pack src/Coder.Object2Report -c:release -o:%cd%
