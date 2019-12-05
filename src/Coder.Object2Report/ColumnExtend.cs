1cddddd586ad5235668bc74cba2b1c3eab7f19d9
luqizheng <luqizheng@126.com>
2019-12-05 15:16:36 +0800

    exception 报错失败

--- src/Coder.Object2Report/ColumnExtend.cs
+++ src/Coder.Object2Report/ColumnExtend.cs
@@ -2,11 +2,13 @@
 using System.ComponentModel;
 using System.Linq;
 using System.Linq.Expressions;
+using System.Reflection;
 
 namespace Coder.Object2Report
 {
     public static class ColumnExtend
     {
+
         /// <summary>
         ///     Refer https://msdn.microsoft.com/en-us/library/dwhawy9k(v=vs.110).aspx
         ///     Refer https://msdn.microsoft.com/en-us/library/0c899ak8(v=vs.110).aspx
@@ -24,7 +26,7 @@ namespace Coder.Object2Report
         {
             if (headerTitle == null)
                 throw new ArgumentNullException(nameof(headerTitle));
-            if (headerTitle == null)
+            if (expression == null)
                 throw new ArgumentNullException(nameof(expression));
             var column = new Column<T, TResult>(headerTitle, expression);
             report.Columns.Add(column);
@@ -50,12 +52,12 @@ namespace Coder.Object2Report
             switch (expression.Body.NodeType)
             {
                 case ExpressionType.MemberAccess:
-                    var memberExpresion = (MemberExpression) expression.Body;
+                    var memberExpresion = (MemberExpression)expression.Body;
 
 
                     var attrData = (from item in memberExpresion.Member.CustomAttributes
-                        where item.AttributeType == typeof(DisplayNameAttribute)
-                        select item).FirstOrDefault();
+                                    where item.AttributeType == typeof(DisplayNameAttribute)
+                                    select item).FirstOrDefault();
                     if (attrData != null && attrData.NamedArguments.Any())
                         return attrData.NamedArguments[0].TypedValue.Value.ToString();
                     return memberExpresion.Member.Name;
